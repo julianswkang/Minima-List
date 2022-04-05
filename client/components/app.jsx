d23a8c43b941ce25e47716770ebcc77d16b7a0cd
Julian Kang <julianswkang@gmail.com>
2022-04-05 18:00:41 -0400

    now loading react components

---
+++ client/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component{
+
+
+  render(){
+    return(
+      <div id='app'>
+        <h1>THE HANDY DANDY TO-DO LIST!</h1>
+      </div>
+    )
+  }
+
+}
+
+export default App;
