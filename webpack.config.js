2b02512b8f875983a376733577b0d287dd2054c7
Julian Kang <julianswkang@gmail.com>
2022-04-04 23:22:16 -0400

    added to webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+  entry: path.join(__dirname, 'index.js'),
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html')
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
