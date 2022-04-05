3cd1dfa2f0ea3aa17b48b9d8a4400a38ae30d87c
Julian Kang <julianswkang@gmail.com>
2022-04-05 18:10:42 -0400

    created itemCreator component, listDisplay container

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ListDisplay from '../containers/listDisplay.jsx';
+import ItemCreator from './itemCreator.jsx';
+
+
 
 class App extends React.Component{
-
 
   render(){
     return(
       <div id='app'>
         <h1>THE HANDY DANDY TO-DO LIST!</h1>
+        <ItemCreator />
+        <ListDisplay />
       </div>
     )
   }
---
+++ client/components/itemCreator.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render} from 'react-dom';
+
+class ItemCreator extends React.Component{
+
+}
+
+export default ItemCreator;
