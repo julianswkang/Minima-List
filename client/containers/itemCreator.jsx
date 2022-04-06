b0fcf7cdae6b50ab0ca1dc61b85a6158ec62a06c
Julian Kang <julianswkang@gmail.com>
2022-04-06 19:51:25 -0400

    completed rudimentary styling

--- client/containers/itemCreator.jsx
+++ client/containers/itemCreator.jsx
@@ -5,22 +5,23 @@ const ItemCreator = (props) => {
   
   return (
     <div className='itemCreator'>
-      <form onSubmit={((e) => props.handleSubmit(e))}>
-      <label>To Do: </label>
-      <input type='text' value={props.text} onChange={((e) => props.handleChange(e))} required/>
-      
-      <label>
-        Priority Level: 
-        <select onChange={((e) => props.handleChange(e))}>
-          <option value='null'>Select One</option>
-          <option value='High'>High</option>
-          <option value='Moderate'>Moderate</option>
-          <option value='Low'>Low</option>
-        </select>
-      </label>
-      <input type='submit' value='Add'/>
-      </form>
-
+      <div className='form'>
+        <form onSubmit={((e) => props.handleSubmit(e))}>
+        <label>To Do: </label>
+        <input id='formText' type='text' value={props.text} onChange={((e) => props.handleChange(e))} required/>
+        <label>
+          Priority Level: 
+          <select onChange={((e) => props.handleChange(e))}>
+            <option value='null'>Select One</option>
+            <option value='High'>High</option>
+            <option value='Moderate'>Moderate</option>
+            <option value='Low'>Low</option>
+          </select>
+        </label>
+        <input type='submit' value='Add' id="addButton"/>
+        </form>
+      </div>
+  
       <div className="pointsBox">
         Points: {props.points}
       </div>
