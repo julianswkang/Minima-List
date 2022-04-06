e3d7adf21e031b1cbb6c70308d803d378ac17ee6
Julian Kang <julianswkang@gmail.com>
2022-04-06 17:17:30 -0400

    updating itemCreator

--- client/containers/itemCreator.jsx
+++ client/containers/itemCreator.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-class ItemCreator extends React.Component{
-
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return (
-     <div className='itemCreator'>
-       <form onSubmit={((e) => this.props.handleSubmit(e))}>
-        <label>To Do: </label>
-        <input type='text' value={this.props.text} onChange={((e) => this.props.handleChange(e))} required/>
-        
-        <label>
-          Priority Level: 
-          <select onChange={((e) => this.props.handleChange(e))}>
-            <option value='null'>Select One</option>
-            <option value='High'>High</option>
-            <option value='Moderate'>Moderate</option>
-            <option value='Low'>Low</option>
-          </select>
-        </label>
-        <input type='submit' value='Add'/>
+const ItemCreator = (props) => {
+  
+  return (
+    <div className='itemCreator'>
+      <form onSubmit={((e) => tprops.handleSubmit(e))}>
+      <label>To Do: </label>
+      <input type='text' value={props.text} onChange={((e) => props.handleChange(e))} required/>
+      
+      <label>
+        Priority Level: 
+        <select onChange={((e) => props.handleChange(e))}>
+          <option value='null'>Select One</option>
+          <option value='High'>High</option>
+          <option value='Moderate'>Moderate</option>
+          <option value='Low'>Low</option>
+        </select>
+      </label>
+      <input type='submit' value='Add'/>
       </form>
 
       <div className="pointsBox">
-        Points: {this.props.points}
+        Points: {props.points}
       </div>
-     </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default ItemCreator;
--- client/containers/listDisplay.jsx
+++ client/containers/listDisplay.jsx
@@ -3,43 +3,36 @@ import { render } from 'react-dom';
 import ListItem from '../components/listItem.jsx'
 
 
-class ListDisplay extends React.Component{
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    let listLength = this.props.list.length;
-    let propsList = this.props.list;
-    const list = [];
-    for (let i = 0; i < listLength; i++){
-      const {listItem, priority, date, _id} = propsList[i];
-      list.push(
-        <ListItem 
-          todo={listItem} 
-          priority={priority} 
-          date={date} 
-          key={_id}
-          id={_id}
-          handleDelete={this.props.handleDelete}
-        />
-      )
-    }
-    // console.log('render invoked!');
-    return (
-      
-      <div className='listBox'>
-        <div className='listTitle'>
-          <h3>To-Do's to do: </h3>
-        </div>
-        <ol className='todoList'>
-          {list}
-        </ol>
-        
-      </div>
-
+const ListDisplay = (props) => {
+  
+  let listLength = props.list.length;
+  let propsList = props.list;
+  const list = [];
+  for (let i = 0; i < listLength; i++){
+    const {listItem, priority, date, _id} = propsList[i];
+    list.push(
+      <ListItem 
+        todo={listItem} 
+        priority={priority} 
+        date={date} 
+        key={_id}
+        id={_id}
+        handleDelete={props.handleDelete}
+      />
     )
   }
+
+  return (
+    <div className='listBox'>
+      <div className='listTitle'>
+        <h3>To-Do's to do: </h3>
+      </div>
+      <ol className='todoList'>
+        {list}
+      </ol>
+    </div>
+  )
 }
+
 
 export default ListDisplay;
