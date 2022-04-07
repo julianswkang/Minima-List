839733aad1999591ca62b1abcd78498e6216e4bf
Julian Kang <julianswkang@gmail.com>
2022-04-07 12:12:17 -0400

    fully functioning with hooks

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -53,13 +53,30 @@ const App = (props) => {
     setItemList(list);
   }
 
-  
+  async function handleDelete(id, priority){
+    const pointSystem = {
+      "High": 5,
+      "Moderate": 3,
+      "Low": 1
+    };
+    setPoints (points + pointSystem[priority]);
+
+    const response = await fetch('/update', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        _id: id
+      })
+    });
+    const list = await response.json();
+    setItemList(list);
+  }
 
   return (
     <div id='app'>
-        
       <h1>THE HANDY DANDY TO-DO LIST!</h1>
-
       <ItemCreator 
         text={listItem} 
         handleSubmit={handleSubmit} 
@@ -67,33 +84,12 @@ const App = (props) => {
         handleChangePriority={setPriority}
         points={points}
       />
-
       <ListDisplay 
-        // handleDelete={this.handleDelete} 
+        handleDelete={handleDelete} 
         list={itemList}
       />
-
     </div>
   )
 }
-
-
-//   /*
-//   ****** THIS PROVIDES FUNCTIONALITY THAT ALLOWS USER TO REMOVE A TO-DO ITEM *******
-//   NOTE: THIS WILL WORK TO DELETE THE ITEM FROM THE DATABASE
-//   */
-//   handleDelete(id, priority){
-//     // console.log('this one needs to be deleted: ', id);
-//     // this will provide details of the point system and 
-//     const pointSystem = {
-//       "High": 5,
-//       "Moderate": 3,
-//       "Low": 1
-//     };
-//     this.setState((prevState) =>({
-//       points: prevState.points + pointSystem[priority]
-//     }))
-
-
 
 export default App;
