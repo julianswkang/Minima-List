53358a5d027aeeb1f678f81221950994bbe1e3f7
Julian Kang <julianswkang@gmail.com>
2022-05-15 22:19:32 -0400

    proper use of one to many relationship of list in userschema, now using a listschema

--- server/models/user.js
+++ server/models/user.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const listSchema = new Schema({
+  todo: {type: String, required: true},
+  priority: {type: String, required: true},
+  date: {type: Date, default: Date.now()}
+})
+
 const userSchema = new Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  list: [{
-    todo: {type: String, required: true},
-    priority: {type: String, required: true},
-    date: {type: Date, default: Date.now()}
-  }],
+  list: [listSchema],
   sessionId: {type: Schema.Types.ObjectId, ref: 'Session'},
   points: Number
 })
+
+
 
 const User = mongoose.model('User', userSchema);
 
