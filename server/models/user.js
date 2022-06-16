480f08240faa6b814161272fde6a11fed1890496
Julian Kang <julianswkang@gmail.com>
2022-06-15 22:17:54 -0400

    added bcrypt hashing of password prior to saving to database

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const BCRYPT_SALT = Number(process.env.BCRYPT_SALT);
 
 const Schema = mongoose.Schema;
 
@@ -16,8 +19,18 @@ const userSchema = new Schema({
   points: Number
 })
 
-
-
 const User = mongoose.model('User', userSchema);
+
+//adding bcrypt salt hashing to the current password to store a hash in the database
+userSchema.pre('save', function(next) {
+  try {
+    const salt = bcrypt.genSaltSync(BCRYPT_SALT);
+    this.password = bcrypt.hashSync(this.password, salt);
+    return next();
+  } catch (err){
+    return next(err);
+  }
+})
+
 
 module.exports = User;
