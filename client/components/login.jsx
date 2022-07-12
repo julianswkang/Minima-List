40d3e0ca2c69046a9f7ae6a0656b1a16a01b71e6
Julian Kang <julianswkang@gmail.com>
2022-07-12 09:28:05 -0400

    attempting to deploy

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,43 +1,55 @@
-import React, {useState} from 'react';
-import Header from '../containers/header.jsx';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import Header from "../containers/header.jsx";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 //Component that provides functionality to log in
 //Houses its own internal state properties for username and password
 //props={handleSetUser(), handleSubmit()}
 const Login = (props) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   let navigate = useNavigate();
 
-  async function handleSubmit(e){
+  async function handleSubmit(e) {
     e.preventDefault();
-    try{
-      await axios.post('/auth/login', {
+    console.log(username, password);
+    try {
+      await axios.post("/auth/login", {
         username,
-        password
-      })
+        password,
+      });
       props.handleSetUser(username);
-      setUsername('');
-      setPassword('');
-      navigate('/');
-    }
-    catch(err){
-      console.log('there was an error signing up user in database! ', err);
+      setUsername("");
+      setPassword("");
+      navigate("/");
+    } catch (err) {
+      console.log("there was an error signing up user in database!!! ", err);
     }
   }
   return (
     <div id="login">
       <Header />
-      <p style={{textAlign:'center'}}>Log in page!</p>
-      <form id='login-inputs' onSubmit={((e) => handleSubmit(e))}>
-        <input type='text' id='login-username' placeholder='Enter Username' onChange={(e) => setUsername(e.target.value)} required/>
-        <input type='password' id='login-password' placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} required/>
-        <input type='submit' id='login-button' value='Log in!'/> 
+      <p style={{ textAlign: "center" }}>Log in page!</p>
+      <form id="login-inputs" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          id="login-username"
+          placeholder="Enter Username"
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          id="login-password"
+          placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input type="submit" id="login-button" value="Log in!" />
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- server/server.js
+++ server/server.js
@@ -1,52 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser')
-const updateRouter = require('./routes/update.js');
-const authRouter = require('./routes/auth.js');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env')})
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const updateRouter = require("./routes/update.js");
+const authRouter = require("./routes/auth.js");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 app.listen(3000, () => {
-  console.log('LISTENING ON PORT 3000');
+  console.log("LISTENING ON PORT 3000");
 });
 /*
  ****** CONNECTING TO MONGODB ******
  */
 //need to utilize env file
 const MONGO_URI = process.env.MONGODB_CONNECTIONSTRING;
-mongoose.connect(MONGO_URI, {
-  // options for the connect method to parse the URI
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // sets the name of the DB that our collections are part of
-  dbName: 'minimalist'
-})
-  .then(() => console.log('Connected to Mongo DB.'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(MONGO_URI, {
+    // options for the connect method to parse the URI
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // sets the name of the DB that our collections are part of
+    dbName: "minimalist",
+  })
+  .then(() => console.log("Connected to Mongo DB."))
+  .catch((err) => console.log(err));
 
 /*
- ******* HANDLE PARSING INTO REQUEST BODY ****** 
-*/
+ ******* HANDLE PARSING INTO REQUEST BODY ******
+ */
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 //app.use(cors());
 
 /*
-* ***** DEFINING ROUTES *******
-*/
-app.use('/update', updateRouter);
-app.use('/auth', authRouter);
+ * ***** DEFINING ROUTES *******
+ */
+app.use("/update", updateRouter);
+app.use("/auth", authRouter);
 
 /*
  ****** ROUTE HANDLER TO RESPOND WITH THE MAIN APPLICATION ****** 
  Note: only to be used when in production mode, since development mode uses webpack server
 */
-if (process.env.NODE_ENV === 'production'){
-  app.use('/', express.static(path.join(__dirname, '../build')));
-  app.get('/', (req,res) => {
-    return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "../build")));
+  app.get("/", (req, res) => {
+    return res
+      .status(200)
+      .sendFile(path.join(__dirname, "../client/index.html"));
   });
 }
 
@@ -54,19 +57,19 @@ if (process.env.NODE_ENV === 'production'){
  ******* CATCH-ALL ROUTE HANDLER FOR UNKNOWN ROUTES *******
  */
 // catch-all route handler for any requests to an unknown route
-app.use('/*', (req, res) => res.status(404).send('NOT A ROUTE'));
+app.use("/*", (req, res) => res.status(404).send("NOT A ROUTE"));
 
 /*
-****** GLOBAL ERROR HANDLER ******
-*/
+ ****** GLOBAL ERROR HANDLER ******
+ */
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 500,
-    message: { err: 'An error occurred' },
-  }
+    message: { err: "An error occurred" },
+  };
 
   const errObj = Object.assign({}, defaultErr, err);
   console.log(errObj.log);
   return res.status(errObj.status).json(errObj.message);
-})
+});
